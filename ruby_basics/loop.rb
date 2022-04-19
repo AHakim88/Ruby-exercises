3a31d673d7fb6dd759e063a690dc3134da056bef
Abdul Hakim <hakim88@gmail.com>
2022-04-19 22:29:59 +0800

    commit

---
+++ ruby_basics/loop.rb
@@ -0,0 +1,40 @@
+# x = gets.chomp.to_i
+# while x >= 0
+#   puts x 
+#   x -= 1 
+# end 
+
+# puts "Done"
+
+# x = gets.chomp.to_i
+
+# until x < 0
+# puts x 
+# x -= 1
+# end 
+
+# puts "Done!"
+
+# loop do 
+#   puts "Do you want to do it again?"
+#   answer = gets.chomp
+#   if answer != 'Y'
+#     break
+#   end 
+# end 
+
+# x = gets.chomp.to_i
+
+# for i in 1..x do 
+#   puts x - i
+# end 
+
+# puts "Done!"
+
+# x = [1,2,3,4,5]
+
+# for i in x.reverse do 
+#   puts i
+# end 
+
+# puts "Done!"
