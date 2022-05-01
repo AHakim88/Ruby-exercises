49300ca71aa21bfcf2b269cd3bfe60584331b678
Abdul Hakim <hakim88@gmail.com>
2022-05-01 15:09:47 +0800

    commit on 'main'

---
+++ loop.rb
@@ -0,0 +1,41 @@
+# loop do
+#   puts 'Should I stop looping?'
+#   answer = gets.chomp
+#   break if answer == "yes"
+#   puts "Incorrect answer. Please answer 'yes'."
+# end
+
+# say_hello = true
+# counter = 0
+
+# while counter <  5 
+#   counter += 1
+#   puts "Hello!"
+# end
+
+# say_hello = true
+# count = 0
+
+# while say_hello
+#   count += 1
+#   puts 'Hello!'
+#   say_hello = false if count == 5
+# end 
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# new_array = []
+# count = 0 
+
+# while count < 5
+# new_array << numbers.sample
+# count += 1
+# end
+# puts new_array
+
+# numbers = []
+
+# while numbers.size < 5 
+#   numbers << rand(100)
+# end 
+
+# puts numbers
